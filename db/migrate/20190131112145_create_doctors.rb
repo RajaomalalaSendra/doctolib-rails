72843371b7b9d5644755def9e1a39c109701f881
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-31 15:11:47 +0300

    adding something

--- db/migrate/20190131112145_create_doctors.rb
+++ db/migrate/20190131112145_create_doctors.rb
@@ -1,7 +1,10 @@
 class CreateDoctors < ActiveRecord::Migration[5.2]
   def change
     create_table :doctors do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :speciality
+      t.integer :postal_code
       t.timestamps
     end
   end
